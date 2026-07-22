6507ab4e00ccc52005e8805dfae145258749ec3d
Khang <baokhang18123@gmail.com>
2026-07-22 12:15:21 +0700

    Add SummerNote

---
+++ Models/Summernote.cs
@@ -0,0 +1,26 @@
+namespace App.Models;
+
+public class Summernote
+{
+    public Summernote(string iDEditor, bool loadLibrary = true)
+    {
+        IDEditor = iDEditor;
+        LoadLibrary = loadLibrary;
+    }
+
+    public string IDEditor { get; set; }
+    public bool LoadLibrary { get; set; }
+
+    public int Height { get; set; } = 120;
+
+    public string Toolbar { get; set; } =
+        @"[
+                    ['style', ['style']],
+                    ['font', ['bold', 'underline', 'clear']],
+                    ['color', ['color']],
+                    ['para', ['ul', 'ol', 'paragraph']],
+                    ['table', ['table']],
+                    ['insert', ['link', 'picture', 'video']],
+                    ['view', ['fullscreen', 'codeview', 'help']]
+                ]";
+}
